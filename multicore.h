1a2bce9ef17a68e033f6e76901edd574b589bf25
Raymond Deng <1107450@gmail.com>
2023-09-23 22:28:13 -0700

    add mcs interface

--- multicore.h
+++ multicore.h
@@ -41,4 +41,18 @@ private:
     thread_local int mySlotIndex;
 };
 
+class MCSLock {
+public:
+    MCSLock();
+    void Lock();
+    void Unlock();
+
+private:
+    struct Node {
+        bool locked = false;
+        Node* next = nullptr;
+    };
+    alignas(64) std::atomic<Node*> tail_;
+};
+
 #endif
